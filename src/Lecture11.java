019d9bff04e3fc93bf16c4d6d0a59d21e56098eb
Your Name <you@example.com>
2024-02-15 08:13:29 -0800

    Initial Commit

---
+++ src/Lecture11.java
@@ -0,0 +1,35 @@
+class Lecture11 {
+    public static void main(String[] args) {
+        System.out.println("Hello from lecture 11");
+
+        // Self-Check Problems:
+        // Section 13.4: Case Study: Implementing Merge Sort
+
+        // 30. Trace the complete execution of the merge sort algorithm when called on
+        // each array below.
+        // Show the sub-arrays that are created by the algorithm and show the merging of
+        // sub-arrays into larger sorted arrays.
+
+        // A. {29, 17, 3, 94, 46, 8, –4, 12}
+        // B. {6, 5, 3, 7, 1, 8, 4, 2}
+        // C. {33, 14, 3, 95, 47, 9, –42, 13}
+
+        // Exercises:
+
+        // 11. Using the following arrays, trace the complete execution of the merge
+        // sort algorithm when called on each array.
+        // Show the subarrays that are created by the algorithm and show the merging of
+        // subarrays into larger sorted arrays.
+
+        // A. int[] numbers3 = {8, 5, –9, 14, 0, –1, –7, 3};
+        // B. int[] numbers4 = {15, 56, 24, 5, 39, –4, 27, 10};
+
+        // 13. Using the following arrays, trace the complete execution of the merge
+        // sort algorithm when called on each array.
+        // Show the subarrays that are created by the algorithm and show the merging of
+        // subarrays into larger sorted arrays.
+
+        // A. int[] numbers5 = {22, 44, 11, 88, 66, 33, 55, 77};
+        // B. int[] numbers6 = {–3, -6, -1, -5, 0, -2, -4, -7};
+    }
+}
